72a17427b020f722a709fc50eacb2d3689fccd82
Matt <mdg408@nyu.edu>
2017-02-14 14:54:14 -0500

    Stylize auth form with modal

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -5,8 +5,8 @@ export default class SessionForm extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-          username: "",
-          password: ""
+          username: "Username",
+          password: "Password"
       };
       this.handleSubmit = this.handleSubmit.bind(this);
       this.handleInput = this.handleInput.bind(this);
@@ -24,9 +24,10 @@ export default class SessionForm extends React.Component {
   render(){
     const { loggedIn, formType, errors, processForm } = this.props;
 
-    const header = formType === 'signup' ? "Sign Up" : "Log In";
+    const header = formType === 'signup' ? "Sign up" : "Log in";
     const linkRoute = formType === 'signup' ? "/login" : "/signup";
     const linkText = formType === 'signup' ? "Login" : "Signup";
+    const linkPromptText = formType === 'signup' ? "Already have an account? " : "Don't have an account? "
     let errorMessages = [];
     if(errors.length > 0){
       errorMessages = errors.map( (error, idx) => {
@@ -37,27 +38,26 @@ export default class SessionForm extends React.Component {
 
       if(!loggedIn){
         return (
-          <div id="session-form">
-            <h1>{header}</h1>
-            <h3><Link to={linkRoute}>{linkText}</Link></h3>
-            <ul className="error-modal">
-              {errorMessages}
-            </ul>
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                Username
-                <input id="username" type="text" onChange={this.handleInput}/>
-              </label>
-              <br />
+          <div>
+            <div id="session-form">
+              <h1 className="session-form-header">{header} to inConcert</h1>
 
-              <label>
-                Password
-                <input id="password" type="password" onChange={this.handleInput}/>
-              </label>
-              <br />
+              <ul className="error-modal">
+                {errorMessages}
+              </ul>
+              <form onSubmit={this.handleSubmit}>
 
-              <input type="submit" value="Submit" />
-            </form>
+                  <input className="session-input" id="username" type="text" onChange={this.handleInput} value={this.state.username}/>
+                <br />
+                  <input className="session-input" id="password" type="password" onChange={this.handleInput} value={this.state.password}/>
+                <br />
+
+                <input className="submit-session" type="submit" value={header} />
+                <input className="submit-guest" type="submit" value="Guest Demo" />
+                <h3 className="change-form">  {linkPromptText}<Link className="change-form-link" to={linkRoute}>{linkText}</Link></h3>
+              </form>
+            </div>
+            <div className="modal-screen"></div>
           </div>
         )} else { return null; }
       }
