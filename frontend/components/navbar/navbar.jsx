244c6a992a5c448988e2c5d8fc882ea11ea4b68d
Matt <mdg408@nyu.edu>
2017-02-19 18:17:10 -0500

    Start a dropdown demnu

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -10,16 +10,25 @@ export default class Navbar extends React.Component {
         <div id="nav-bar-logged-in">
           <Link to="/"><img className="logo" src="https://s3.amazonaws.com/inconcert-dev/inConcert+logo_2.png" /></Link>
 
-          <form onSubmit={logout}>
-            <input type="submit" className="signout-button" value="Sign Out" />
-          </form>
-          
+
           <ul className="nav-right">
             <form>
               <input type="text" className="search-bar" value ="Search videos, artists and more"/>
               <span><i className="fa fa-search search-logged-in" aria-hidden="true"></i></span>
             </form>
-            <li><img className="nav-avatar" src={currentUser.avatar_url} /></li>
+
+            <li className="nav-dropper">
+              <img className="nav-avatar" src={currentUser.avatar_url} />
+              <ul id="nav-dropdown" >
+              	<li>
+              		<span className="user-welcome">Welcome, {currentUser.username}</span>
+              	</li>
+              	<li onClick={logout}>
+              		<button id="logout-button">Log Out</button>
+              	</li>
+              </ul>
+            </li>
+
             <li><Link to="/upload"><button className="upload-button"><i className="fa fa-upload" aria-hidden="true"></i>Upload</button></Link></li>
             <li>
             </li>
