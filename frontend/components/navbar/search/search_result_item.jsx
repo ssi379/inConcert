3bedd68a268214cc44024f4dcfae5a4b39f9ac09
Matt <mdg408@nyu.edu>
2017-02-22 16:35:48 -0500

    Style search results

---
+++ frontend/components/navbar/search/search_result_item.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class SearchResultItem extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+      const { video } =  this.props;
+      const linkPath = `/videos/${video.id}`
+      return(
+        <div className="search-result-item-container">
+
+          <div className="video-index-item-thumbnail">
+            <Link to={linkPath}><img src={video.thumbnail_url} width={320} height={180} /></Link>
+          </div>
+
+          <div>
+            <h3 className="video-index-item-title title-ellipsis">{video.title}</h3>
+          </div>
+
+          <div className="index-uploader-info">
+            <Link to="/">
+              <img className="index-uploader-avatar" src={video.user.avatar_url} />
+              <span className="index-item-uploader">{video.user.username}</span>
+            </Link>
+            <span className="index-item-views">{video.views.toLocaleString()} plays</span>
+          </div>
+        </div>
+      )
+
+  }
+}
--- frontend/components/navbar/search/search_results.jsx
+++ frontend/components/navbar/search/search_results.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchResultItem from './search_result_item';
 
 export default class SearchResults extends React.Component{
   constructor(props){
@@ -21,15 +22,31 @@ export default class SearchResults extends React.Component{
 
     if(typeof this.props.searchResults === "undefined"){ return null }
     return this.props.searchResults.map((video, idx) => {
-      return(<li key={`search-${idx}`}>{video.title}</li>)
+      return(<SearchResultItem video={video} key={`search-${idx}`} />)
     })
   }
 
+  renderResultHeader(){
+    if(typeof this.props.searchResults === "undefined"){
+      return null
+    } else {
+      return(
+        <h1 className="search-result-header">
+          {this.props.searchResults.length} results for <strong className="result-query">{this.props.location.search.slice(7)}</strong>
+        </h1>
+      )
+    }
+
+  }
+
   render(){
+
     return(
-      <div>
-        <h1>HELLO</h1>
-        {this.renderSearchResultItems()}
+      <div className="search-results-container">
+        {this.renderResultHeader()}
+        <div className="search-results-index">
+          {this.renderSearchResultItems()}
+        </div>
       </div>
     )
   }
