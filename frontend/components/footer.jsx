df4bc91ae4b23070b40a209820442963abc2f427
Matt <mdg408@nyu.edu>
2017-03-07 13:07:53 -0500

    Add portfolio link to footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return(
     <footer id="site-footer">
       <ul className="footer-links">
-        <li><a href="#">Website</a></li>
+        <li><a href="http://www.matthewdylangoldman.com">Website</a></li>
         <li><a href="https://github.com/MatthewDG/inConcert">Github</a></li>
         <li><a href="https://www.linkedin.com/in/matthew-goldman-a6aaa6115/">LinkedIn</a></li>
       </ul>
