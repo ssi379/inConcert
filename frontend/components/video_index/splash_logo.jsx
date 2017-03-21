8b9ec6a5c1f3b89155b5466435962d0f2701463e
Matt <mdg408@nyu.edu>
2017-03-21 14:17:22 -0400

    Brief fade on main logo

--- frontend/components/video_index/splash_logo.jsx
+++ frontend/components/video_index/splash_logo.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const SplashLogo = () => (
   <div className="animated-logo-container">
 
-    
+
     <img src={window.images.logoLeft4} id="left4" />
     <img src={window.images.logoLeft3} id="left3" />
     <img src={window.images.logoLeft2} id="left2" />
     <img src={window.images.logoLeft1} id="left1" />
 
 
-    <img src={window.images.logoMain}/>
+    <img src={window.images.logoMain} id="main-logo"/>
 
     <img src={window.images.logoRight1} id="right1" />
     <img src={window.images.logoRight2} id="right2" />
