2dd6dbb754e948222b0bd898e5ad6d89e80e61f4
Matt <mdg408@nyu.edu>
2017-03-23 14:49:05 -0400

    Database level username uniqueness validation

---
+++ db/migrate/20170323183444_replace_user_index.rb
@@ -0,0 +1,6 @@
+class ReplaceUserIndex < ActiveRecord::Migration
+  def change
+    remove_index(:users, :username)
+    add_index(:users, :username, unique: true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221164730) do
+ActiveRecord::Schema.define(version: 20170323183444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20170221164730) do
   end
 
   add_index "users", ["session_token"], name: "index_users_on_session_token", using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "videos", force: :cascade do |t|
     t.string   "title",                                  null: false
